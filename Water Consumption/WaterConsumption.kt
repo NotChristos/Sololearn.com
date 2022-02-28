a7768630f026ba331ae892fa62afce403add67d1
Kristina Alex <96779254+NotChristos@users.noreply.github.com>
2022-02-28 21:20:10 +0300

    Update WaterConsumption.kt

--- Water Consumption/WaterConsumption.kt
+++ Water Consumption/WaterConsumption.kt
@@ -1,6 +1,7 @@
 fun main(args: Array<String>) {
+    println("Укажите, сколько лет вы живёте в этом доме:")
     var years = readLine()!!.toInt()
     years *= 15
     years *= 365
-    println(years)
+    println("$years литров воды было израсходованнно за это время")
 }
