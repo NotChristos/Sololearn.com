9ac252972803f1d858533e2ec34e86574eff9658
Kristina Alex <96779254+NotChristos@users.noreply.github.com>
2022-03-07 15:33:36 +0300

    Update FilteringNames.kt

--- FilteringNames/FilteringNames.kt
+++ FilteringNames/FilteringNames.kt
@@ -1,6 +1,6 @@
 fun main(args: Array<String>) {
     println("Enter the first letter of the name:")
-    val letter = readLine()!![0].uppercaseChar()
+    val letter = readLine()!![0].uppercaseChar() //пользовательсий ввод с переводом Char в верхний регистр
     val names =
         arrayOf("John", "David", "Amy", "James", "Amanda", "Dave", "Bob", "Billy", "Bobby", "Diana", "Lenny", "Gina")
     names.forEach { i ->
